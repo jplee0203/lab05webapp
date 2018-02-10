3874e9c9655dd92356fb5172010856f63190a08e
jplee0203 <jplee0203@gmail.com>
2018-02-10 14:46:08 -0800

    added newly 02/10

--- src/App.js
+++ src/App.js
@@ -5,82 +5,81 @@ import mySocket from "socket.io-client";
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            myImg:require("./images/robot.png"),
-            myImg2:require("./images/robot1.png"),
-            allusers:[],
-            myId:null
+        this.state={
+            myImg: require("./img/img1.png"),
+            myImg2: require("./img/img2.png"), 
+            allUsers:[],
+            myId: null
         }
+        
         this.handleImage = this.handleImage.bind(this);
     }
     
     componentDidMount(){
+        this.socket = mySocket("https://lab05socket.herokuapp.com/");
         
-        this.socket=mySocket("https://lab05socket.herokuapp.com/");
         this.socket.on("userjoined", (data)=>{
-           this.setState({allusers:data
-                         }) 
+            this.setState({
+                allUsers: data
+            }); 
         });
         
         this.socket.on("yourid", (data)=>{
             this.setState({
-                myId:data
-            })
-        })
-        this.socket.on("newmove", (data)=>{
-           this.refs["u"+data.id].style.left = data.x + "px";
-           this.refs["u"+data.id].style.top = data.y +"px";
+                myId: data
+            });
         });
         
+        this.socket.on("newmove", (data)=>{
+            this.refs["u"+data.id].style.left = data.x+"px"; 
+            this.refs["u"+data.id].style.top = data.y+"px";
+            this.refs["u"+data.id].src = data.src;
+        });
         
-        this.refs.thedisplay.addEventListener("mousemove", (ev)=>{
+        this.refs.theDisplay.addEventListener("mousemove", (ev)=>{
             if(this.state.myId === null){
+                //FAIL
                 return false;
             }
             
-            this.refs["u"+this.state.myId].style.left = ev.pageX + "px";                                    
-            this.refs["u"+this.state.myId].style.top = ev.pageY + "px"; 
-//            this.refs["u"+data.id].src = data.src;
+            this.refs["u"+this.state.myId].style.left = ev.pageX+"px";
+            this.refs["u"+this.state.myId].style.top = ev.pageY+"px";
+            
             
             this.socket.emit("mymove", {
-                x:ev.pageX+"px",
-                y:ev.pageY+"px",
-                id:this.state.myId,
-                src:this.refs["u"+this.state.myId].src
-            })
-        });
+                x: ev.pageX,
+                y: ev.pageY,
+                id: this.state.myId,
+                src: this.refs["u"+this.state.myId].src
+            });
+        });       
     }
     
     handleImage(evt){
-        this.refs["u"+this.state.myId].src = evt.target.src;
+        this.refs["u"+this.state.myId].src = evt.target.src;        
     }
     
-  render() {
-      
-      var allimgs = this.state.allusers.map((obj,i)=>{
-          return(
-            <img ref={"u"+obj} className="allImgs" src={this.state.myImg} height={50} key={i}/>
-          )
-      });
-      
-    return (
-      <div className="App">
-
-        <div ref="thedisplay" className="display">
-            {allimgs}
-           
-        </div>
+    render() {
         
-        <div className="control">
-            {this.state.myId}
-                <img ref="myImg" id="myImg" src={this.state.myImg} height={50} onClick={this.handleImage}/>
-                <img ref="myImg2" id="myImg2" src={this.state.myImg2} height={50} onClick={this.handleImage}/>
-        </div>
+        var allimgs = this.state.allUsers.map((obj, i)=>{
+            return(
+                <img ref={"u"+obj} className="allImgs" src={this.state.myImg} height={50} key={i} />   
+            );
+        });
         
-        
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <div ref="theDisplay" id="display">
+                    {allimgs}
+                </div>
+                <div id="controls">
+                    {this.state.myId}
+                    <img src={this.state.myImg} height={50} onClick={this.handleImage} />
+                    <img src={this.state.myImg2} height={50} onClick={this.handleImage} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
